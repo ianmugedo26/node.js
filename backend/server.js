34e2a334cf4fb6fd937f5074974e28094808a540
ianmugedo26 <mugedoian@gmail.com>
2026-02-08 23:06:40 +0300

    Update

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const connectDB = require('.config/db')
 const { errorHandler } = require ('./middleware/errorMiddleware');
 
 const port = 5000;
-
 const app = express();
+connectDB();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
