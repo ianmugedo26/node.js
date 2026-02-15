a97d2d8db2f3835ffe10d8fab68258c8d6139e43
ianmugedo26 <mugedoian@gmail.com>
2026-02-15 23:56:16 +0300

    secure Todos Rest APIs

--- backend/controllers/todosControllers.js
+++ backend/controllers/todosControllers.js
@@ -1,20 +1,22 @@
 const asyncHandler = require('express-async-handler');
+const User = require("../models/user");
 const Todo = require('../models/todoModel');
 
 const getTodos = asyncHandler(async (req, res) => {
-    const todos = await Todo.find();
+    const todos = await Todo.find({ user: req.used.id });
     res.status(200).json({message: 'Get todos'})
 });
-const setTodos = (req, res) => {
+const setTodos =asyncHandler(async (req, res) => {
     if(!req.body.text) {
         res.status(400);
         throw new Error('Subject not found');
     }
     const todo = await Todo.create({
         text: res.body.text,
+        user: req.user.id, //associate the todos with the logged-in user
     });
     res.status(200).json({message: 'Set todos'})
-};
+});
 
 const updateTodos =  asyncHandler(async(req, res) => {
     const todo = await Todo.findById(res.params.id);
@@ -25,8 +27,18 @@ const updateTodos =  asyncHandler(async(req, res) => {
 
     }
     const updateTodos = await Todos.findByIdAndUpdate(req.param.id, req.body, {
-        new: true;
+        new: true,
     })
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
+    if (todo.new.toString() !== user.id) {
+        res.status(401);
+        throw new Error("User not Authorised.");
+    }
     res.status(200).json({message: `Update todos ${req.params.id}`})
 });
 
@@ -36,6 +48,15 @@ const deleteTodos = asyncHandler(async (req, res) => {
     if(!todo) {
         res.status(400);
         throw new Error("Subject not found");
+    }
+    const user = await User.findById(req.user.id);
+    if (!user) {
+        res.status(401);
+        throw new Error("User not found");
+    }
+    if (todo.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error("User not authorised");
     }
 
     await todo.remove();
--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 const { getTodos, setTodos, updateTodos, deleteTodos } = require ('../controllers/todosControllers')
 
-router.route('/').get(getTodos).post(setTodos)
-router.route('/:id').put(updateTodos).delete(deleteTodos)
+router.route("/").get(protect, getTodos).post(protect, setTodos);
+router.route("/:id").put(protect, updateTodos).delete(protect, deleteTodos);
+router.route('/').get(getTodos).post(setTodos);
+router.route('/:id').put(updateTodos).delete(deleteTodos);
 
 module.exports = router;
